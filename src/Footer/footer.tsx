f7b0b3bd10aa86e232390b7679da2841d57a669c
Rakesh Naskar <rakeshnaskar499@gmail.com>
2025-08-08 00:37:22 +0530

    little more changes in footer

--- src/Footer/footer.tsx
+++ src/Footer/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full bg-gray-700 text-gray-100 text-sm text-center py-3 shadow-inner border-gray-500 rounded-t-lg">
-      © 2025 Your Project Name. All rights reserved.
+      © 2025 Rakesh Naskar. All rights reserved.
     </footer>
   );
 }
